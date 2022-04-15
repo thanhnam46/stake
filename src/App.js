4b4de7349fadccb65ea9c91342a99f01498d2b74
nam <thanhnam97@gmail.com>
2022-04-15 15:02:54 +0700

    react UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3'
+import FestakedWithReward from './artifacts/contracts/FestakedWithReward.sol/FestakedWithReward.json'
+
+const FestakedWithRewardAddress = '0xbE7E299bB3c2c3B0e436A54935365B9aFF26EB04'
 
 function App() {
+  let walletAddress='000'
+
+  async function requestAccount() {
+    walletAddress = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    console.log(walletAddress)    
+  }  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <img className='logo' src='https://imgur.com/Qxw1soD.jpeg' alt='logo'></img>
+        <a href='#' className='links'>Transaction</a>
+        <a href='#' className='links'>Staking Options</a>
+        <a href='#' className='connectWalletBtn links' onClick={requestAccount}>Connect Wallet</a>
       </header>
+      <div className='container'>
+        <div className='content'>
+          <div className='stakeBox'>
+            <p>SPO-BSC Short Term</p>
+            <p>Connected to BSC network</p>
+            <p>YOUR ADDRESS</p>
+            <p>{walletAddress}</p>
+          </div>
+          <div className='poolInfor'>
+            <ul>
+              <li>Your staked balance</li>
+              <li>Staking cap </li>
+              <li>Staked so far</li>
+              <li>Maturity reward</li>
+              <li>Early rewards</li>
+              <li>Staking contribution close</li>
+              <li>Early withdraw open</li>
+              <li>Maturity at</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/web3.js
@@ -0,0 +1,5 @@
+import Web3 from "web3/dist/web3.min"
+
+const web3 = new Web3(window.web3.currentProvider)
+
+export default web3
