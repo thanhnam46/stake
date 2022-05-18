042a1edfcd8d5c94d91d088083a0d5a9c2eaa882
nam <thanhnam97@gmail.com>
2022-05-18 10:46:27 +0700

    style & new contract address

--- scripts/deploy-FestakedWithReward.js
+++ scripts/deploy-FestakedWithReward.js
@@ -22,16 +22,16 @@ async function main() {
     },
   });
   const stakingContract = await FestakedWithReward.deploy(
-    "Spores piloting mini staking pool", //Name
+    "SPO staking pool May 2022", //Name
     // "0x476f7BcbC4058d4a0E8C0f9a6Df1fdcF675FAC83", //Testnet token address
     "0x8357c604c5533fa0053BeAaA1494Da552ceA38f7", //Mainnet token address
     // "0x476f7BcbC4058d4a0E8C0f9a6Df1fdcF675FAC83", //Testnet rewardTokenAddress_
     "0x8357c604c5533fa0053BeAaA1494Da552ceA38f7", //Mainnet rewardTokenAddress_
-    "1651053600", //stakingStarts_
-    "1651831200", //stakingEnds_
-    "1651831201", //withdrawStarts_
-    "1652263200", //withdrawEnds_
-    "500000000000000000000000", //stakingCap_
+    "1652950800", //stakingStarts_
+    "1655542800", //stakingEnds_
+    "1658134800", //withdrawStarts_
+    "1663318800", //withdrawEnds_
+    "100000000000000000000000000", //stakingCap_
     //{gasLimit: 10000000}    //rinkeby
     {gasLimit: 10000000}    //bscTestnet
     );
--- src/components/HOC/hoc.js
+++ src/components/HOC/hoc.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Web3 from "web3/dist/web3.min"
 import FestakedWithReward from "../../artifacts/contracts/FestakedWithReward.sol/FestakedWithReward.json"
 import tokenContract from "../../artifacts/contracts/tokenContract/tokenContract.json"
