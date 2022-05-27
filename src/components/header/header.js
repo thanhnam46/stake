287b29f0c07c06203b53a7d780c3d74ca7654468
nam <thanhnam97@gmail.com>
2022-05-27 16:49:11 +0700

    change transaction link at header from testnet to mainnet

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./header.css"
 import withWallet from "../HOC/hoc"
 function Header (props) {
-  const txLink = `https://testnet.bscscan.com/address/${props.account}#tokentxns`
+  const txLink = `https://bscscan.com/address/${props.account}#tokentxns`
   props.onAccountChange()
 
   return (
